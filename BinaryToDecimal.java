ccd73528d8a2f2b87f4325f5e84703c613fe483c
ANSHUL BANSAL <anshulbansal1123@gmail.com>
2025-10-09 05:53:46 +0530

    Add files via upload

---
+++ BinaryToDecimal.java
@@ -0,0 +1,18 @@
+public class BinaryToDecimal {
+    public static void main(String[] args) {
+        String binary = "1011"; 
+        int decimal = binaryToDecimal(binary);
+        System.out.println("Binary: " + binary + " to  Decimal: " + decimal);
+    }
+    public static int binaryToDecimal(String binary) {
+        int decimal = 0;
+        int length = binary.length();
+        for (int i = 0; i < length; i++) {
+            char bit = binary.charAt(length - 1 - i); // Start from rightmost bit
+            if (bit == '1') {
+                decimal += Math.pow(2, i);
+            }
+        }
+        return decimal;
+    }
+}
